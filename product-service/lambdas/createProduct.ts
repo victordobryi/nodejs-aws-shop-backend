1942106c0014d4422418669147022b243c14d5e6
bazla <dobr1nya@mail.ru>
2023-11-27 01:52:34 +0300

    feat: add lambdas logs

--- product-service/lambdas/createProduct.ts
+++ product-service/lambdas/createProduct.ts
@@ -3,6 +3,8 @@ import { buildResponse } from '../utils/buildResponse';
 import { createOne } from '../model/products.model';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
+  console.log(`createProduct lambda => event: ${JSON.stringify(event)}`);
+
   const { body } = event;
 
   if (!body) {
