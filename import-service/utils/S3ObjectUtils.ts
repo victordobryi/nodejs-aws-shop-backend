3e11d37e46d788570146a782a7adeea836acdf97
bazla <dobr1nya@mail.ru>
2023-12-05 16:58:26 +0300

    feat: add importProductsFile lambda test

--- import-service/utils/S3ObjectUtils.ts
+++ import-service/utils/S3ObjectUtils.ts
@@ -6,7 +6,7 @@ import {
   ListObjectsCommand,
   PutObjectCommand,
 } from '@aws-sdk/client-s3';
-import { client as s3Client } from '../client.js';
+import { client as s3Client } from '../client';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Readable } from 'stream';
 
